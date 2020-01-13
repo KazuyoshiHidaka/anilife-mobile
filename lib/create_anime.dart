fc4d0e003b073f2eca2a87be3007f5a29a3ca598
KazuyoshiHidaka <kazuyoshi.first@gmail.com>
2020-01-13 16:14:48 +0900

    add animeTitleForm

--- lib/create_anime.dart
+++ lib/create_anime.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
 class CreateAnimePage extends StatelessWidget {
+  final _titleFormController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
         title: Text("アニメを登録"),
       ),
-      // body: ,
+      body: Form(
+        autovalidate: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              autofocus: true,
+              controller: _titleFormController,
+              maxLength: 50,
+              decoration: InputDecoration(
+                hintText: "タイトル",
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    _titleFormController.clear();
+                  },
+                  icon: Icon(Icons.clear),
+                ),
+              ),
+              validator: (String value) {
+                return value.isEmpty ? "タイトルを入力して下さい" : null;
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
