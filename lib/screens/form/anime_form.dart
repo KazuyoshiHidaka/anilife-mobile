d91a1eb8f6ae4b02795a275ecdbac8e7a1880287
KazuyoshiHidaka <kazuyoshi.first@gmail.com>
2020-01-16 02:11:19 +0900

    add lib/screens/form directory

---
+++ lib/screens/form/anime_form.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:anilife_mobile/screens/form/datatime_picker_form_field.dart';
+import 'package:anilife_mobile/screens/form/date_picker_form_field.dart';
+import 'package:anilife_mobile/screens/form/notify_repeat_interval_form_field.dart';
+import 'package:anilife_mobile/screens/form/notify_timing_form_field.dart';
+import 'package:anilife_mobile/screens/form/time_picker_form_field.dart';
+import 'package:anilife_mobile/screens/form/title_form_field.dart';
+
+class AnimeForm extends StatefulWidget {
+  const AnimeForm({
+    Key key,
+    @required GlobalKey<FormState> formKey,
+  })  : _formKey = formKey,
+        super(key: key);
+  final GlobalKey<FormState> _formKey;
+
+  @override
+  _AnimeFormState createState() => _AnimeFormState();
+}
+
+class _AnimeFormState extends State<AnimeForm> {
+  static const _dividerIndent = Divider(height: 1, indent: 8);
+  static const _divider = Divider(height: 1);
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: widget._formKey,
+      autovalidate: true,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              top: 5,
+              left: 8,
+              right: 8,
+              bottom: 20,
+            ),
+            child: TitleFormField(),
+          ),
+          _divider,
+          // DatePickerFormField(),
+          // _dividerIndent,
+          // TimePickerFormField(),
+          DateTimePickerFormField(),
+          _dividerIndent,
+          NotifyTimingFormField(),
+          _dividerIndent,
+          NotifyRepeatIntervalFormField(),
+          _divider,
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/form/datatime_picker_form_field.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/intl.dart';
+
+class DateTimePickerFormField extends StatefulWidget {
+  @override
+  _DateTimePickerFormFieldState createState() =>
+      _DateTimePickerFormFieldState();
+}
+
+class _DateTimePickerFormFieldState extends State<DateTimePickerFormField> {
+  DateTime _dateTimeState = DateTime.now();
+
+  String _datetimeFormat(DateTime datetime) {
+    var _date;
+    if (datetime.year == DateTime.now().year) {
+      _date = DateFormat.MMMEd("ja").format(datetime);
+    } else {
+      _date = DateFormat.yMMMEd("ja").format(datetime);
+    }
+    return "$_date  " + DateFormat.Hm("ja").format(datetime);
+  }
+
+  String _dateTimeValidator() {
+    return _dateTimeState.isBefore(DateTime.now()) ? "\n現在より前の日時は指定できません" : "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField(
+      initialValue: _datetimeFormat(_dateTimeState),
+      validator: (_) {
+        String _validate = _dateTimeValidator();
+        if (_validate.isNotEmpty) {
+          // Scaffold.of(context).showSnackBar(SnackBar(
+          //   content: Text(""),
+          // ));
+        }
+      },
+      builder: (state) {
+        return FlatButton(
+          onPressed: () async {
+            DatePicker.showDateTimePicker(
+              context,
+              minTime: DateTime.now(),
+              currentTime: _dateTimeState,
+              locale: LocaleType.jp,
+              onConfirm: (value) {
+                _dateTimeState = value;
+                state.didChange(_datetimeFormat(value));
+              },
+            );
+          },
+          child: ListTile(
+            title: Text(
+              "視聴する日時",
+            ),
+            subtitle: Text(
+              "${state.value}",
+            ),
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 8,
+              vertical: 0,
+            ),
+          ),
+          padding: EdgeInsets.all(0),
+        );
+      },
+    );
+  }
+}
